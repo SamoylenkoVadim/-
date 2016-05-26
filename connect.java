c78c59c4c9e879f9d86bab55ca0346de1fbd28d2
SamoylenkoVadim <vsam26@yandex.ru>
2016-05-26 04:59:52 +0300

    соединение с базами

--- connect.java
+++ connect.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -5,60 +8,69 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Connect {
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-	public static void main(String[] args) throws SQLException {
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import oracle.jdbc.OracleResultSet;
+
+public class Connect {
+	
+	public Connection connectFrom 	= null;
+	public Connection connectCfg	= null;
+	public Connection connectTo		= null;
+	
+	public Connect(){
+		
+		findDriver();
+		setConnection();
+		
+	}
+	
+	private void findDriver(){
 		
 		System.out.println("-------- Oracle JDBC Connection Testing ------");
-
 		try {
-
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-
 		} catch (ClassNotFoundException e) {
-
 			System.out.println("Where is your Oracle JDBC Driver?");
 			e.printStackTrace();
 			return;
-
 		}
-
 		System.out.println("Oracle JDBC Driver Registered!");
-
-		Connection connection = null;
-
+		
+	}
+	
+	private void setConnection(){
+		
 		try {
-
-			connection = DriverManager.getConnection(
+			connectFrom = DriverManager.getConnection(
 					"jdbc:oracle:thin:@localhost:1521:SBTPRACTICE", "esblog",
+					"qwe123");
+			connectCfg = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:SBTPRACTICE", "esblog_cfg",
 					"qwe123");
 
 		} catch (SQLException e) {
-
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
 			return;
-
 		}
-
-		if (connection != null) {
+		if ((connectFrom != null)&&(connectCfg != null)) {
 			System.out.println("You made it, take control your database now!");
 		} else {
 			System.out.println("Failed to make connection!");
 		}
 		
-		PreparedStatement st = connection.prepareStatement("select * from SBT1 where EVENT_ID = 256021319621");
-	    ResultSet r1 =st.executeQuery();
-
-	     r1.next();
-	     System.out.println( r1.getString(2));
+		System.out.println("Connect:: All connections with Oracle DB is established successfully!");
 		
-		/*DatabaseMetaData md = connection.getMetaData();
-		ResultSet rs = md. //select * from SBT1 where EVENT_ID=256021319621;
-		while (rs.next()) {
-		  System.out.println(rs.getString("EVENT_ID"));
-		}*/
-		System.out.println("That is all");
 	}
+
 		
 }
+
+
