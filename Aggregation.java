323464b9f8331e10675f430fd0a6eae63606df1b
SamoylenkoVadim <vsam26@yandex.ru>
2016-06-01 04:54:56 +0300

    (1)присутствует запись в БД, зациклен поиск

--- Aggregation.java
+++ Aggregation.java
@@ -3,7 +3,7 @@ import java.io.StringReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
+import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,17 +17,20 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class Aggregation {
-	
-	
 
 	public static void main(String[] args) throws SQLException, ParserConfigurationException, SAXException, IOException {
 		
 		Connect connect = new Connect();
 		
 		PreparedStatement st;
+		PreparedStatement stTo;
+		PreparedStatement stCfg;
 
 		st = connect.connectFrom.prepareStatement("select * from SBT1 where EVENT_ID = 256021319621");
+		//stTo = connect.connectTo.prepareStatement("insert into PERSONS (MSG_TYPE, LAST_NAME, FIRST_NAME, MIDDLE_NAME, CARD_NUM) values ('qqq', 5, 'eee', 'rrr', 08)");
+		//stTo.executeQuery();
 		ResultSet r1 =st.executeQuery();
+		
 	
 		r1.next();
 	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -41,47 +44,59 @@ public class Aggregation {
 	    NodeList nList = doc.getElementsByTagName("MSG_BODY");
 	    String MSG_TYPE = nList.item(0).getFirstChild().getNodeName();
 	    
+	    String request = "select * from CONFIGURATIONS where MSG_TYPE = '" + MSG_TYPE + "'";
+	    System.out.println(request+ "  ");
+	    stCfg = connect.connectCfg.prepareStatement(request);
+	    ResultSet cfg =stCfg.executeQuery();
+	    
+	    StructureForRecursion obj = new StructureForRecursion();
+	   
+	    while(cfg.next()){
+	    	
+	    	String from = cfg.getString("GO_FROM");
+		    String to 	= cfg.getString("GO_TO");
+		    String [] argFrom 	= from.split("/");
+		    String [] argTo	 	= to.split("/");
+		    
+		    Element eElement = (Element) doc.getElementsByTagName(argFrom[0]).item(0);
+
+		    downTo(eElement,argFrom,1,obj);
+		    
+		    //obj.prepareRow(argTo);
+		    
+		    //exportToDB(connect, obj.vector, argTo);
+		    
+		    //System.out.println(obj.vector.toString());
+		    System.out.print(obj.getArrayNodeNames());
+		    System.out.println(obj.getArrayText());
+	   }
+		    
+	   
+	    
+	    
+	    
+
 	    
 	    // тут будет функция по получению правила по обработке соощения из конфигов. допустим получили::
 	    
 	    //String from = "BillingPayExecRq/CardAcctId/CustInfo/PersonInfo/PersonName/FirstName";
-	    String from = "BillingPayExecRq/RecipientRec/Requisites/Requisite*where NameVisible = КБК/NameBS";
+	    //String from = "BillingPayExecRq/RecipientRec/Requisites/Requisite*where NameVisible = КБК/NameBS";
 	    //String from = "BillingPayExecRq/RecipientRec/Requisites/Requisite*where NameVisible = КБК/AttributeLength/MinLength";
-	    //String from = "BillingPayExecRq/RecipientRec/Requisites/Requisite/NameVisible";
+	    // String from = "BillingPayExecRq/RecipientRec/Requisites/Requisite/NameVisible";
 	    
-	    String to 	= "PERSONS/FIRST_NAME";
+	   // String to 	= "PERSONS/FIRST_NAME";
 	    
-	    String [] argFrom 	= from.split("/");
-	    String [] argTo	 	= to.split("/");
 	    
-	    Element eElement = (Element) doc.getElementsByTagName(argFrom[0]).item(0);
 	    
-	    StructureForRecursion obj = new StructureForRecursion();
 	    
-	    downTo(eElement,argFrom,1,obj);
-	    System.out.println(obj.vector.toString());
 	    System.out.println("That is all");
 
-	}
-	
-	private static Node search(NodeList children, String arg){
+	}	
 		
-		Node	current = null;
-		for (int i = 0; i < children.getLength(); i++) {
-	         current = children.item(i);
-	         if (current.getNodeName() == arg)
-	        	 return current;
-	    }
-		return null;
-	}
-	
-	
-	
 		private static void downTo(Element eElement, String[] arg, int i, StructureForRecursion obj){
 
 			if (arg[i].indexOf("*") != -1) {
-				obj = checkKey(eElement, arg[i], obj);
-				eElement = obj.eElement;
+				eElement = checkKey(eElement, arg[i]);
 				i++;
 			}
 			
@@ -92,25 +107,21 @@ public class Aggregation {
 				eElement = (Element) nList.item(k);
 				
 				if (i < arg.length - 1)
-						downTo(eElement, arg, i+1, obj);
+					downTo(eElement, arg, i+1, obj);
 				else
-					obj.pullToArray(eElement.getTextContent());
+					obj.pullToArray(eElement);
 			}
 			return;
 		}
 		
-		
-		
-		private static StructureForRecursion checkKey(Element el, String str, StructureForRecursion obj){
+
+		private static Element checkKey(Element el, String str){
 			
 			int sep = str.indexOf("*");
-			obj.eElement = el;
 
 				String nameCurrentNode = str.substring(0, str.indexOf("*"));
 				String nameCheckField = str.substring(str.indexOf("where ",sep) + 6,str.indexOf(" =",sep));
 				String value = str.substring(str.indexOf("= ") + 2, str.length());
-				obj.arg = nameCurrentNode;
-				
 				
 				NodeList nList  = el.getElementsByTagName(nameCurrentNode);
 				int lengthList = nList.getLength();
@@ -118,73 +129,30 @@ public class Aggregation {
 					el = (Element) nList.item(k);
 
 					if (el.getElementsByTagName(nameCheckField).item(0).getTextContent().equals(value)){
-
-						obj.eElement = el;
-						return obj;
+						return el;
 					}
 				}
 
-				return obj;
+				return el;
 			}
-
 		
+		private static void exportToDB(Connect connect, Vector<Element> vector, String[] argTo){
+			
+			String request = "insert into " + argTo[0] + " (";
+			for (int i = 1; i < argTo.length; i++)
+				request = request + argTo[i] + ", ";
+			
+			PreparedStatement stTo;
+			/*try {
+				stTo = connect.connectTo.prepareStatement("insert into PERSONS (MSG_TYPE, LAST_NAME, FIRST_NAME, MIDDLE_NAME, CARD_NUM) values ('qqq', 5, 'eee', 'rrr', 08)");
+				stTo.executeQuery();
+			} catch (SQLException e) {
+				System.out.println("ExportToDB: Something happened.. Data did not record in the DB ");
+				e.printStackTrace();
+			}*/
+
+
+			
+		}
+
 }
-
-/* eElement = (Element) eElement.getElementsByTagName(argFrom[1]).item(0);
-Element eElement = (Element) eElement.getElementsByTagName(argFrom[2]).item(0)getFirstChild().getTextContent();
-eElement = (Element) eElement.getElementsByTagName(argFrom[3]).item(0);
-eElement = (Element) eElement.getElementsByTagName(argFrom[4]).item(0);
-eElement = (Element) eElement.getElementsByTagName(argFrom[5]).item(0);*/
-
-//System.out.println(eElement.getTextContent());
-//System.out.println(eElement.getFirstChild().getTextContent());
-
-//eElement = downTo(eElement,argFrom,1,null);
- // Element [] elements = downTo(eElement,argFrom,1, new Element [50],0);
-//System.out.println(argFrom[argFrom.length-1]);
-
-//System.out.println(elements[0].getTextContent());
-	//eElement = (Element) eElement.getElementsByTagName("CardAcctId").item(0);
-
-//eElement = (Element) eElement.getElementsByTagName(arg[i]).item(0);
-
-/* 
-for (int i = 0; i < children.getLength(); i++) {
-     Node current = children.item(i);
-     if (current.getNodeName() == "MSG_BODY"){
-    	 current = current.getFirstChild();
-    	 System.out.println(current.getNodeName());
-     }
-}
-
-Node current = null;
-for (int i = 0; i < children.getLength(); i++) {
-     current = children.item(i);
-     if (current.getNodeName() == "MSG_BODY"){
-    	 System.out.println("BODY");
-    	 break;
-    	 }
-}
-
-NodeList List = current.getChildNodes();
-
-for (int i = 0; i < List.getLength(); i++) {
-     current = List.item(i);
-     if (current.getNodeName() == "BillingPayExecRq")
-    	 System.out.println("EXEC!!!");
-}*/
-
-/*NodeList list = search(children, "MSG_BODY").getChildNodes();
-Node one = search(list,"BillingPayExecRq");
-System.out.println(one.getFirstChild().getTextContent());*/
-
-//System.out.println(search(list,"SPName").get);
-
-
-//NodeList nList = doc.getElementsByTagName("BillingPayExecRq").item(0).getChildNodes();
-
-
-
-//System.out.println(eElement.getElementsByTagName("CardAcctId").item(0).getFirstChild().getNodeName());
-
-//System.out.println(nList.item(0).getNodeName());
