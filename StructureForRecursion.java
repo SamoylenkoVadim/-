75036de608d8f20094eb693475ac321aee2c3c20
SamoylenkoVadim <vsam26@yandex.ru>
2016-06-03 06:34:30 +0300

    new

--- StructureForRecursion.java
+++ StructureForRecursion.java
@@ -8,17 +8,20 @@ import org.w3c.dom.Element;
 		Vector<String> vNodeName	= new Vector<String>(0,1);
 		Vector<String> vText		= new Vector<String>(0,1);
 		Vector<String[]> request	= new Vector<String[]>(0,1);
+		Vector<String> inserts		= new Vector<String>(0,1);
+		String newStr 				= null;
 	
-		public void pullToArray(Element el){
+		public void pullToArray(String text){
 			 
-			 vNodeName.addElement(el.getNodeName());
-			 vText.addElement(el.getTextContent());
+			 //vNodeName.addElement(el.getNodeName());
+			 vText.addElement(text);
 			 
 		}
 		
 		public String getArrayText(){
 			
-			String str = vText.toString(); 
+			String str = vText.toString();
+
 			vText.removeAllElements(); 
 			return str.substring(1, str.length()-1);
 			
@@ -60,8 +63,60 @@ import org.w3c.dom.Element;
 									str.substring(str.indexOf(" ('"), str.indexOf("')")) + "', '" + getArrayText() + "')";
 				vArr[i+1] = newStr;
 			}
-					
+
+		}
+	
+		public void setInserts(String[] to){
+
+			if (inserts.capacity() == 0){
+				newStr = "insert into "+ to[0] + " (" + to[1] + ") values (')";
+
+			}else{
+				newStr = newStr.substring(0, newStr.indexOf(") values")) + ", " + to[1] + ") values " + newStr.substring(newStr.indexOf(") values"));	
+			}
 			
+
+			if (vText.capacity() == 1){
+				
+				if (inserts.capacity() > 0){
+					for (int i = 0; i < inserts.capacity(); i++){
+						String currStr = inserts.get(i);
+						currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(0) + "')";
+						inserts.set(i, currStr);
+					}
+				}
+				
+				if (inserts.capacity() == 0){
+					System.out.println( newStr.substring(0, newStr.indexOf("')")));
+					String currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(0) + "')";
+					inserts.addElement(currStr);
+				}
+
+			}
+			//------------------------------------------------------------
+			
+			if (vText.capacity() > 1){
+				
+				if (inserts.capacity() > 0){
+					for (int i = 0; i < inserts.capacity(); i++){
+						String currStr = inserts.get(i);
+						currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(i) + "')";
+						inserts.set(i, currStr);
+					}
+				}
+				
+				if (inserts.capacity() == 0){
+					
+					for (int i = 0; i < vText.capacity(); i++){
+						String currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(i) + "')";
+						inserts.addElement(currStr);
+					}
+					
+				}
+				
+			}
+			for (int i = 0; i < inserts.capacity(); i++)
+				System.out.println(inserts.get(i));
 		}
 		
 	
