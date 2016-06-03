938795592a1c440b87c45642850ebd59a1a4e357
SamoylenkoVadim <vsam26@yandex.ru>
2016-06-04 00:18:58 +0300

    Будет еще пару изменений

--- StructureForRecursion.java
+++ StructureForRecursion.java
@@ -9,11 +9,11 @@ import org.w3c.dom.Element;
 		Vector<String> vText		= new Vector<String>(0,1);
 		Vector<String[]> request	= new Vector<String[]>(0,1);
 		Vector<String> inserts		= new Vector<String>(0,1);
+		Vector<Vector>	vector		= new Vector<Vector>(0,1);
 		String newStr 				= null;
 	
 		public void pullToArray(String text){
-			 
-			 //vNodeName.addElement(el.getNodeName());
+
 			 vText.addElement(text);
 			 
 		}
@@ -21,75 +21,61 @@ import org.w3c.dom.Element;
 		public String getArrayText(){
 			
 			String str = vText.toString();
-
 			vText.removeAllElements(); 
 			return str.substring(1, str.length()-1);
 			
 			
 		}
+				
 		
-		public String getArrayNodeNames(){
+		public void setInserts(String[] argTo){
 			
-			return vNodeName.toString();
+			for (int i = 0; i < argTo.length; i++){
+				argTo[i] = argTo[i].replaceFirst(" ","");
+				String [] arrayTo = argTo[i].split("/");
+				
+				inserts(arrayTo);
+			}
+			
+			vText.clear();
 			
 		}
 	
-		public void prepareRow(String[] to){
-
-			boolean flag = true;
-			String[] vArr = null;
+		public void inserts(String[] to){
 			
-			int i = 0;
-			for (i = 0; i < request.capacity(); i++){
-				vArr = request.get(i);
-				if (vArr[0].equals(to[0])){
-					flag = false;
-					break;
-				}				
-			}
+			String extraPoint = ", '";
 			
-			if (flag){
-				
-				String[] newArr = new String [2];
-				newArr[0] = to[0];
-				String newStr = "insert into "+ to[0] + " (" + to[1] + ") values ('"+ getArrayText() + "')"; 
-				newArr[1] = newStr;
-				request.add(newArr);
-				
+			if (vText.isEmpty()){ // вообще такое не должно происходить, если случается, то это не правильно.
+				return;
 			}
-			if (!flag){
-				String str = vArr[i+1];
-				String newStr = str.substring(0, str.indexOf(") values")) + ", " + to[1] + ") values" + 
-									str.substring(str.indexOf(" ('"), str.indexOf("')")) + "', '" + getArrayText() + "')";
-				vArr[i+1] = newStr;
-			}
-
-		}
-	
-		public void setInserts(String[] to){
-
+				
 			if (inserts.capacity() == 0){
-				newStr = "insert into "+ to[0] + " (" + to[1] + ") values (')";
+				newStr = "insert into "+ to[0] + " (" + to[1] + ") values ()";
+				extraPoint = "'";
 
 			}else{
-				newStr = newStr.substring(0, newStr.indexOf(") values")) + ", " + to[1] + ") values " + newStr.substring(newStr.indexOf(") values"));	
+				newStr = newStr.substring(0, newStr.indexOf(") values")) + ", " + to[1]  + newStr.substring(newStr.indexOf(") values"));	
 			}
 			
-
+			
 			if (vText.capacity() == 1){
 				
 				if (inserts.capacity() > 0){
 					for (int i = 0; i < inserts.capacity(); i++){
 						String currStr = inserts.get(i);
-						currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(0) + "')";
+						
+						currStr = newStr.substring(0, newStr.indexOf(")", newStr.indexOf("values"))) + extraPoint + vText.get(0) + "')";
+						newStr = currStr;
 						inserts.set(i, currStr);
 					}
 				}
 				
 				if (inserts.capacity() == 0){
-					System.out.println( newStr.substring(0, newStr.indexOf("')")));
-					String currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(0) + "')";
+					//System.out.println( newStr.substring(0, newStr.indexOf("')")));
+					String currStr = newStr.substring(0, newStr.indexOf(")", newStr.indexOf("values"))) + extraPoint + vText.get(0) + "')";
+					newStr = currStr;
 					inserts.addElement(currStr);
+					
 				}
 
 			}
@@ -99,8 +85,9 @@ import org.w3c.dom.Element;
 				
 				if (inserts.capacity() > 0){
 					for (int i = 0; i < inserts.capacity(); i++){
-						String currStr = inserts.get(i);
-						currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(i) + "')";
+						String currStr = inserts.get(i);	
+						currStr = newStr.substring(0, newStr.indexOf(")", newStr.indexOf("values"))) + extraPoint + vText.get(i) + "')";
+						//newStr = currStr;
 						inserts.set(i, currStr);
 					}
 				}
@@ -108,13 +95,16 @@ import org.w3c.dom.Element;
 				if (inserts.capacity() == 0){
 					
 					for (int i = 0; i < vText.capacity(); i++){
-						String currStr = newStr.substring(0, newStr.indexOf("')")) + "'" + vText.get(i) + "')";
+						String currStr = newStr.substring(0, newStr.indexOf(")", newStr.indexOf("values"))) + extraPoint + vText.get(i) + "')";
+						//newStr = currStr;
 						inserts.addElement(currStr);
 					}
 					
 				}
 				
 			}
+			vText.clear();
+			//vText.removeAllElements(); 
 			for (int i = 0; i < inserts.capacity(); i++)
 				System.out.println(inserts.get(i));
 		}
