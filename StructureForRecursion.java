b437384ecf422a4a29b0ee41ffc0626b2a445d1a
SamoylenkoVadim <vsam26@yandex.ru>
2016-06-04 19:23:42 +0300

    Задание выполнено

--- StructureForRecursion.java
+++ StructureForRecursion.java
@@ -5,13 +5,13 @@ import org.w3c.dom.Element;
 	
 	public class StructureForRecursion {
 	
-		Vector<String> vNodeName	= new Vector<String>(0,1);
+		//Vector<String> vNodeName	= new Vector<String>(0,1);
 		Vector<String> vText		= new Vector<String>(0,1);
 		//Vector<String[]> request	= new Vector<String[]>(0,1);
 		//Vector<String> inserts		= new Vector<String>(0,1);
 		Vector<Vector<String>> 	requests = new Vector<Vector<String>>(0,1);
 		String newStr 				= null;
-		String [] idStr			= new String[1000];
+		//String [] idStr			= new String[1000];
 	
 		public void pullToArray(String text){
 
@@ -37,13 +37,18 @@ import org.w3c.dom.Element;
 					requests.addElement(inserts);
 					
 				}else{				
-					for (int j = 0; i < requests.capacity(); j++){
+					for (int j = 0; j < requests.capacity(); j++){
+
 						if (requests.get(j).get(0).indexOf(arrayTo[0]) > 0){
-							inserts = new Vector<String>(0,1);
-							inserts = formInserts(arrayTo, requests.get(j));
-							requests.set(j, inserts);
-							found = true;
-							break;
+							
+							if (requests.get(j).get(0).indexOf(arrayTo[1]) == -1){
+								inserts = new Vector<String>(0,1);
+								inserts = formInserts(arrayTo, requests.get(j));
+								requests.set(j, inserts);
+								found = true;
+								break;
+							}
+							
 						}						
 					}
 					
@@ -55,6 +60,10 @@ import org.w3c.dom.Element;
 				}
 			}	
 			vText.clear();
+			vText.removeAllElements();
+			vText = null;
+			newStr = null;
+			vText = new Vector<String>(0,1);
 		}
 	
 		public Vector<String> formInserts(String[] to, Vector<String> inserts){
@@ -79,11 +88,12 @@ import org.w3c.dom.Element;
 			if (vText.capacity() == 1){
 				
 				if (inserts.capacity() > 0){
+
 					for (int i = 0; i < inserts.capacity(); i++){
 						String currStr = inserts.get(i);
-						
-						currStr = newStr.substring(0, newStr.indexOf(")", newStr.indexOf("values"))) + extraPoint + vText.get(0) + "')";
-						newStr = currStr;
+						currStr = currStr.substring(0, currStr.indexOf(") values")) + ", " + to[1]  + currStr.substring(currStr.indexOf(") values"));
+						currStr = currStr.substring(0, currStr.indexOf(")", currStr.indexOf("values"))) + extraPoint + vText.get(0) + "')";
+						//newStr = currStr;
 						inserts.set(i, currStr);
 					}
 				}
