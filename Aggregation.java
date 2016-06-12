9b43e1f77c748059fdb4fc34c09b58378ed52aa5
Вадим Самойленко <vsam26@yandex.ru>
2016-06-13 00:42:59 +0300

    Задание выполнено2

--- Aggregation.java
+++ Aggregation.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.StringReader;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,58 +19,76 @@ import org.xml.sax.SAXException;
 
 public class Aggregation {
 
-	public static void main(String[] args) throws SQLException, ParserConfigurationException, SAXException, IOException {
+	public static void main(String[] args) throws Exception  {
 		
-		Connect connect = new Connect();
-		
-		PreparedStatement st;
-		PreparedStatement stTo;
-		PreparedStatement stCfg;
-
-		st = connect.connectFrom.prepareStatement("select * from SBT1");// where EVENT_ID = 256021319621");
-		ResultSet r1 =st.executeQuery();
-		StructureForRecursion obj = new StructureForRecursion();
+		Connect connect 			= new Connect();
+		StructureForRecursion obj 	= new StructureForRecursion();
+		ResultSet r1 = getDataBase("select * from SBT1", connect.connectFrom);
 
 		while (r1.next()){
 			
-			boolean isReqKnown = false;
-			
-		    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		    DocumentBuilder builder = factory.newDocumentBuilder();
-		    InputSource is = new InputSource(new StringReader(r1.getString("EVENT_MSG")));
-		    Document doc = builder.parse(is);
-		    
-		    NodeList nList = doc.getElementsByTagName("MSG_BODY");
-		    String MSG_TYPE = nList.item(0).getFirstChild().getNodeName();
-		    
-		    String request = "select * from CONFIGURATIONS where MSG_TYPE = '" + MSG_TYPE + "'";
-		    //System.out.println(request+ "  ");
-		    stCfg = connect.connectCfg.prepareStatement(request);
-		    ResultSet cfg =stCfg.executeQuery();
-		    //cfg.next();
-		    
-				while(cfg.next()){
-					
-					isReqKnown = true;
+			Document doc 	= getDocumentFromResultStream(r1, "EVENT_MSG");
+		    String request 	= getRequestForConfigDB(doc);
+		    ResultSet cfg 	= getDataBase(request, connect.connectCfg);
 
-			    	String from = cfg.getString("GO_FROM");
-				    String to 	= cfg.getString("GO_TO");
-				    String [] argFrom 	= from.split("/");
-				    String [] argTo	 	= to.split(",");
-				    //System.out.println(to);
-				    Element eElement = (Element) doc.getElementsByTagName(argFrom[0]).item(0);
-				   // System.out.println(from);
-				    downTo(eElement,argFrom,1,obj);
-
-				    obj.setInserts(argTo);
-				    //exportToDB(connect, obj);
-
-			   }
+			while(cfg.next()){
 				
+				aggregation(cfg, doc, obj);
+				
+			}
+			
 		}
 		exportToDB(connect, obj);
 	    System.out.println("That is all");
 	}
+	
+	//---------------------------------------------------------------------------------------------------------------------
+	
+	//---------------------------------------------------------------------------------------------------------------------
+	
+	//---------------------------------------------------------------------------------------------------------------------
+	
+		private static void aggregation(ResultSet cfg, Document doc, StructureForRecursion obj) throws Exception{
+			
+			String from = cfg.getString("GO_FROM");
+		    String to 	= cfg.getString("GO_TO");
+		    String [] argFrom 	= from.split("/");
+		    String [] argTo	 	= to.split(",");
+		    Element eElement = (Element) doc.getElementsByTagName(argFrom[0]).item(0);
+
+		    downTo(eElement,argFrom,1,obj);
+		    obj.setInserts(argTo);
+		    
+		}
+	
+		private static String getRequestForConfigDB(Document doc){
+			
+			NodeList nList = doc.getElementsByTagName("MSG_BODY");
+		    String MSG_TYPE = nList.item(0).getFirstChild().getNodeName();
+		    String request = "select * from CONFIGURATIONS where MSG_TYPE = '" + MSG_TYPE + "'";
+			
+			return request;
+		}
+	
+		private static Document getDocumentFromResultStream(ResultSet r1, String nameColumn) throws Exception  {
+			
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		    DocumentBuilder builder = factory.newDocumentBuilder();
+		    InputSource is = new InputSource(new StringReader(r1.getString(nameColumn)));
+		    Document doc = builder.parse(is);
+			
+			return doc;
+		}
+	
+		private static ResultSet getDataBase(String sqlRequest, Connection connect) throws SQLException{
+			
+			PreparedStatement prSt;
+		
+			prSt = connect.prepareStatement(sqlRequest);  // where EVENT_ID = 256021319621");
+			ResultSet resultSet =prSt.executeQuery();
+			
+			return resultSet;
+		}
 	
 		private static void downTo(Element el, String[] arg, int i, StructureForRecursion obj){
 
@@ -230,7 +249,7 @@ public class Aggregation {
 						for (int j = 0; j < obj.requests.get(i).capacity(); j++){
 							stTo = connect.connectTo.prepareStatement(obj.requests.get(i).get(j));
 							System.out.println(obj.requests.get(i).get(j));
-							stTo.executeQuery();
+							//stTo.executeQuery();
 						}
 					}
 		    } catch (SQLException e) {
@@ -240,6 +259,9 @@ public class Aggregation {
 		}
 
 }
+
+
+
 //String from = "BillingPayExecRq/RecipientRec/Requisites/Requisite*item()/NameVisible";
 //String to = "PERSONS/LAST_NAME, AAA/FIRST_NAME";
 //Node node = doc.getChildNodes().item(0);
